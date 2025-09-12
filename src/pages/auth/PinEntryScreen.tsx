96860d0df46c528bd9a14a08f9a103bb07c1bc32
geodxb <herremangeorginadorao@gmail.com>
2025-09-12 02:43:26 -0600

    Make terminal logo static and centered

--- src/pages/auth/PinEntryScreen.tsx
+++ src/pages/auth/PinEntryScreen.tsx
@@ -317,7 +317,7 @@ const PinEntryScreen = ({ onAuthenticated }: PinEntryScreenProps) => {
             >
               {/* Interactive Brokers Logo Background */}
               <div 
-                className="absolute inset-0 flex items-center justify-center pointer-events-none"
+                className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex items-center justify-center pointer-events-none"
                 style={{ zIndex: 1 }}
               >
                 <img 
